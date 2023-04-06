8c5514713248591cb8f937842d9b84e4cf609e77
sando <codemoochim@gmail.com>
2023-04-07 01:07:21 +0900

    Fix: 사진업로드

---
+++ router/Contents.js
@@ -0,0 +1,31 @@
+// const { Contents } = require("../models/Contents");
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+    // cb(null, "server/uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage }).single("file");
+
+app.post("/images", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.json({
+      success: true,
+      filePath: res.req.file.path,
+      fileName: res.req.file.filename,
+    });
+  });
+});
+
+module.exports = router;
